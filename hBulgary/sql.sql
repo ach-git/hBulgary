679eb51ba955df9247af00839c52f9a9f4fb705c
ach <82474261+ach-git@users.noreply.github.com>
2021-10-24 19:14:43 +0200

    Update sql.sql

--- hBulgary/sql.sql
+++ hBulgary/sql.sql
@@ -3,5 +3,5 @@ INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('lockpick', 'Outils 
 INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('robbedphone', 'Téléphone volé', 1);
 INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('table', 'Tableau', 1);
 INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('jewelry', 'Bijoux', 1);
-INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('tv', 'Télé', 1);
+INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('tv', 'Télévision', 1);
 INSERT INTO `items` (`name`, `label`, `can_remove`) VALUES ('bang', 'Bang', 1);
